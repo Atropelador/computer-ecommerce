461f2a849e4044a66268e380ff169dc68652a312
Marco <marcohuf123@gmail.com>
2024-12-10 12:39:46 +1000

    filter logic is now working

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ products }) {
   return products?.map((product) => (
-    <a key={product.id} href={product.href} className="flex justify-between flex-col">
+    <a key={product.id} href={product.href} className="flex justify-between flex-col border-2 border-solid rounded-lg border-orange-400 p-6">
       <img alt={product.title} src={product.image} className="mb-auto mt-auto self-center" />
       <div>
         <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
--- src/pages/Shopping.jsx
+++ src/pages/Shopping.jsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import Card from "../components/Card";
 import Header from "../components/Header";
-import Categories from "../components/Categories";
+// import Categories from "../components/Categories";
 
 function Shopping() {
   const [products, setProducts] = useState([]);
+  const [activeCategory, setActiveCategory] = useState("all");
+  const categories = [
+    { name: "all", label: "All" },
+    { name: "men's clothing", label: "Mens Clothing" },
+    { name: "women's clothing", label: "Womens Clothing" },
+    { name: "electronics", label: "Electronics" },
+    { name: "jewelery", label: "Jewelery" },
+  ];
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products", { mode: "cors" })
@@ -16,15 +24,52 @@ function Shopping() {
       .catch((error) => console.error(error));
   }, []);
 
+  const filterProducts = (event) => {
+    const name = event.target.getAttribute("data-name");
+    setActiveCategory(name);
+    console.log(name);
+  };
+
+  const filteredProducts = activeCategory === "all" ? products : products.filter((product) => product.category === activeCategory)
+
   return (
     <>
       <Header></Header>
-      <Categories></Categories>
+      <div>
+        <ul className="flex">
+          {categories.map((category, index) => (
+            <li
+              key={index}
+              className={`
+                    flex-1 
+                    text-center 
+                    px-4 
+                    py-2 
+                    border 
+                    border-indigo-600 
+                    bg-gray-100 
+                    text-gray-800 
+                    hover:bg-indigo-600 
+                    hover:text-white 
+                    transition 
+                    duration-300 
+                    ease-in-out
+                    cursor-pointer
+                    ${activeCategory === category.name ? "bg-indigo-600 text-white font-bold" : ""}
+                  `}
+              data-name={category.name}
+              onClick={filterProducts}
+            >
+              {category.label}
+            </li>
+          ))}
+        </ul>
+      </div>
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 className="sr-only">Products</h2>
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            <Card products={products}></Card>
+            <Card products={filteredProducts}></Card>
           </div>
         </div>
       </div>
